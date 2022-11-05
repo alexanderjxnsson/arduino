c496260286a9b8b7767e5305da03e54f1e24b511
alexander jönsson <alexander.jxnsson@gmail.com>
2022-11-05 17:16:09 +0100

    added struct to code, won't run atm

--- free_rtos_parameters_homework/free_rtos_parameters_homework.ino
+++ free_rtos_parameters_homework/free_rtos_parameters_homework.ino
@@ -1,58 +1,41 @@
+// https://www.hackster.io/Niket/tasks-parametertotasks-freertos-tutorial-5-b8a7b7
+
 #include <Arduino_FreeRTOS.h>
 
-#define RED 6
-#define YELLOW 7
-#define GREEN 8
+#define RED_PIN 6
+#define YELLOW_PIN 7
+#define GREEN_PIN 8
 
-typedef struct data_t{
-  uint8_t * _pin;
-  TickType_t _delay = pdMS_TO_TICKS(delay);  
-} sRED, sYELLOW, sGREEN;
 
-typedef int TaskProfiler;
-TaskProfiler REDprofiler;
-TaskProfiler YELLOWprofiler;
-TaskProfiler GREENprofiler;
-
-const TickType_t _50ms = pdMS_TO_TICKS(50);
-const uint8_t *redLED = (uint8_t *) RED;
-const uint8_t *yellowLED = (uint8_t *) YELLOW;
-//const uint8_t *greenLED = (uint8_t t *) GREEN;
+typedef struct LED_struct{
+  uint8_t sPIN;
+  uint8_t sDELAY;
+} sLEDS;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  sLEDS sRED = {RED_PIN, 500};
+  sLEDS sYELLOW = {YELLOW_PIN, 200};
+  sLEDS sGREEN = {GREEN_PIN, 1000};
 
-  xTaskCreate(toggleLED, "Toggle RED", 128, (void *)redLED, 1, NULL);
-  xTaskCreate(toggleLED, "Toggle YELLOW", 128, (void *)yellowLED, 1, NULL);
-  //xTaskCreate(toggleGREEN, "Toggle GREEN", 128, (void *)greenLED, 1, NULL);
+  xTaskCreate(LED_struct, "Toggle LED via struct", 128, (void *)&sRED, 1, NULL);
+  xTaskCreate(LED_struct, "Toggle LED via struct", 128, (void *)&sYELLOW, 1, NULL);
+  xTaskCreate(LED_struct, "Toggle LED via struct", 128, (void *)&sGREEN, 1, NULL);
 }
 
-void toggleLED(void *pvParameters){
-  pinMode(RED, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
-
+void LED_struct(void *xStruct){
   while(1){
-    switch((uint8_t) pvParameters){
-      case RED:{
-        REDprofiler++;
-        Serial.print("RED P = ");
-        Serial.print(REDprofiler);
-        Serial.print("\n");
-      };
-      case YELLOW:{
-        YELLOWprofiler++;
-        Serial.print("YELLOW P = ");
-        Serial.print(YELLOWprofiler);
-        Serial.print("\n");
-      };
-    }
-    digitalWrite((uint8_t)pvParameters, digitalRead((uint8_t)pvParameters)^1);
-    vTaskDelay(_50ms);
- }
+    sLEDS * local_struct = (sLEDS *) xStruct;
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(YELLOW_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+
+    digitalWrite(local_struct->sPIN, HIGH);
+    delay(250);
+    digitalWrite(local_struct->sPIN, LOW);
+    delay(local_struct->sDELAY);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
