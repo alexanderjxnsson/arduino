bb5daddde4331af4152a328f5b9d9f446647ddab
alexander jxnsson <alexander.jxnsson@gmail.com>
2022-11-09 10:47:42 +0100

    free rtos que first done, second created

---
+++ free_rtos_que_with_prios/free_rtos_que_with_prios.ino
@@ -0,0 +1,47 @@
+#include <Arduino_FreeRTOS.h>
+#include "queue.h"
+
+QueueHandle_t iQueue;
+
+void setup() {
+  Serial.begin(9600);
+
+  iQueue = xQueueCreate(5, sizeof(uint32_t));
+
+  xTaskCreate(vSender, "Sender task", 128, NULL, 1, NULL);
+  xTaskCreate(vReceiver, "Reciver task", 128, NULL, 1, NULL);
+
+
+}
+
+void vSender(void* parameter){
+  // create parameter for return value of Queue send
+  BaseType_t qStatus;
+  TickType_t xTickToWait = pdMS_TO_TICKS(100);
+  uint32_t payloadToSend = 2022;
+  while (1) {
+    // send payload to iQueue
+    qStatus = xQueueSend(iQueue, &payloadToSend, xTickToWait);
+    payloadToSend += 10;
+  }
+}
+
+void vReceiver(void* parameter){
+  // declaration to payload will be recieved
+  uint32_t payloadToReceive;
+  BaseType_t qStatus;
+  TickType_t xTickToWait = pdMS_TO_TICKS(100);
+
+  while (1) {
+    qStatus = xQueueReceive(iQueue, &payloadToReceive, xTickToWait);
+    if (qStatus == pdPASS) {
+      Serial.print("Received value is :");
+      Serial.println(payloadToReceive);
+    }
+    else {
+      Serial.println("!!!-Error receving-!!!");
+    }
+  }
+}
+
+void loop() {}
