bac7dec6732ce26bb15c0d2ec7343d6f6bcad1d0
alexander jxnsson <alexander.jxnsson@gmail.com>
2022-11-22 11:00:39 +0100

    Binary semaphore

---
+++ free_rtos_binary_semaphore/free_rtos_binary_semaphore.ino
@@ -0,0 +1,65 @@
+#include <Arduino_FreeRTOS.h>
+#include "semphr.h"
+
+// Create binary semaphore handle
+SemaphoreHandle_t bSemaphore;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Create semaphore
+  bSemaphore = xSemaphoreCreateBinary();
+  if(bSemaphore != NULL){ // Create three tasks
+    xTaskCreate(m1Writer, "Write msg1", 128, NULL, 1, NULL);
+    xTaskCreate(m2Writer, "Write msg2", 128, NULL, 1, NULL);
+    xTaskCreate(m3Writer, "Write msg3", 128, NULL, 1, NULL);
+    xSemaphoreGive(bSemaphore);
+  } else{for(;;);}
+}
+
+void m1Writer(void *  par){
+  BaseType_t bSemStatus;
+
+  while (1) {
+    // Take semaphore
+    bSemStatus = xSemaphoreTake(bSemaphore, portMAX_DELAY);
+    if (bSemStatus == pdPASS) {
+      Serial.println("[1] MSG 1");
+      // Give semaphore
+      xSemaphoreGive(bSemaphore);
+      vTaskDelay(1);
+    } // If
+  } // While
+} // Function
+
+void m2Writer(void * par){
+  BaseType_t bSemStatus;
+
+  while (1) {
+    // Take semahpore
+    bSemStatus = xSemaphoreTake(bSemaphore, portMAX_DELAY);
+    if (bSemStatus == pdPASS) {
+      Serial.println("[2] MSG 2");
+      // Give semaphore
+      xSemaphoreGive(bSemaphore);
+      vTaskDelay(1);
+    } // If
+  } // While
+} // Function
+
+void m3Writer(void * par){
+  BaseType_t bSemStatus;
+
+  while (1) {
+    // Take semahpore
+    bSemStatus = xSemaphoreTake(bSemaphore, portMAX_DELAY);
+    if (bSemStatus == pdPASS) {
+      Serial.println("[3] MSG 3");
+      // Give semaphore
+      xSemaphoreGive(bSemaphore);
+      vTaskDelay(1);
+    } // If
+  } // While
+} // Function
+
+void loop() {}
