f48104e172059f7de228ca9410ec40c6382a77c1
alexander jxnsson <alexander.jxnsson@gmail.com>
2022-11-25 09:09:36 +0100

    Struct added 1.5

--- free_rtos_inlämning_1.5/free_rtos_inlämning_1.5.ino
+++ free_rtos_inlämning_1.5/free_rtos_inlämning_1.5.ino
@@ -11,10 +11,16 @@ typedef struct {
   uint32_t prog_length_s;       // Length of the program
   uint16_t effect_of_heater;    // The effect of
 } sMicrowave;
+
+// Here we've initilaized the different programs of the microwave from the struct
+// First we add program time in seconds, door, lamp, effect and then disc
+sMicrowave defrost_meat = {300, 800};
+sMicrowave defrost_veg = {60, 400};
+sMicrowave general_prog = {30, 800};
+
 #define PRINT_DELAY (pdMS_TO_TICKS(1000)) // Delay set to 1000ms(1sec)
 bool bLamp = true, bHeater = true, bDoor;
 TaskHandle_t heater_Handle, lamp_Handle, disc_Handle, door_Handle;
-uint8_t menu_choice;
 uint8_t door_open_or_close = 1;
 
 void setup() {
@@ -26,8 +32,8 @@ void setup() {
   // Creating our first task for menu and chosing program
   xTaskCreate(door, "Main task to trigger the others", 128, door_open_or_close, 1, &door_Handle);
   xTaskCreate(lamp, "Lamp task", 128, NULL, 1, &lamp_Handle);
-  xTaskCreate(disc, "Disc task calculation", 128, 30, 1, &disc_Handle);
-  xTaskCreate(heater, "Heater task", 128, 800, 1, &heater_Handle);
+  xTaskCreate(disc, "Disc task calculation", 128, &general_prog, 1, &disc_Handle);
+  xTaskCreate(heater, "Heater task", 128, &general_prog, 1, &heater_Handle);
 }
 
 void door(void * param){
@@ -49,10 +55,11 @@ void door(void * param){
   }
 }
 
-void heater(void * param){
-  uint16_t effect = (uint16_t)param;
+void heater(void * input_struct){
+  sMicrowave * local_struct = (sMicrowave *) input_struct;
+
   Serial.print("Heater is set to: ");
-  Serial.println(effect);
+  Serial.println(local_struct->effect_of_heater);
   while (1) {
     if (bHeater == true ) {
       digitalWrite(RED_PIN, HIGH);
@@ -76,30 +83,32 @@ void lamp(void * param){
   } // While
 } // Function
 
-void disc(void * param){
-  uint16_t disc_degree = 0;
-  uint8_t program_seconds = (uint8_t)param;
-  // Initalizing variables for clock timer
-  uint8_t prog_h = (program_seconds / 3600); 
-  uint8_t prog_m = (program_seconds - (3600 * prog_h)) /60;
-  uint8_t prog_s = (program_seconds - (3600 * prog_h) - (prog_m * 60));
+void disc(void * input_struct){
+  sMicrowave * local_struct = (sMicrowave *) input_struct;
 
-  if (program_seconds >= 60) {                      // If the program is 1 min or longer display it as minutes
+  uint16_t disc_degree = 0;
+
+  // Initalizing variables for clock timer
+  uint8_t prog_h = (local_struct->prog_length_s / 3600); 
+  uint8_t prog_m = (local_struct->prog_length_s - (3600 * prog_h)) /60;
+  uint8_t prog_s = (local_struct->prog_length_s - (3600 * prog_h) - (prog_m * 60));
+
+  if (local_struct->prog_length_s >= 60) {                      // If the program is 1 min or longer display it as minutes
     Serial.print("Program length is set to: ");
     Serial.print(prog_h); Serial.print(":");  Serial.print(prog_m); Serial.print(":");  Serial.println(prog_s);
   } // If
   else {                                                        // If the program is less than 60 sec, display it as secnds
     Serial.print("Program length is set to: ");
-    Serial.print(program_seconds);
+    Serial.print(local_struct->prog_length_s);
     Serial.println(" seconds.");
   } // Else
 
-   while(program_seconds){
+   while(local_struct->prog_length_s){
     // Time calculations within the while loop to represent countdown timer
-    prog_h = (program_seconds / 3600); 
-    prog_m = (program_seconds - (3600 * prog_h)) /60;
-    prog_s = (program_seconds - (3600 * prog_h) - (prog_m * 60));
-    program_seconds -= 1;
+    prog_h = (local_struct->prog_length_s / 3600); 
+    prog_m = (local_struct->prog_length_s - (3600 * prog_h)) /60;
+    prog_s = (local_struct->prog_length_s - (3600 * prog_h) - (prog_m * 60));
+    local_struct->prog_length_s -= 1;
 
     // Print time remaining
     Serial.print("Time remaining: ");
@@ -110,7 +119,7 @@ void disc(void * param){
     }
     else{Serial.println(prog_s);}
 
-    if(program_seconds == 0){                       // When prog_length reach 0 we exit the while loop
+    if(local_struct->prog_length_s == 0){                       // When prog_length reach 0 we exit the while loop
       Serial.println("PROGRAM DONE!");
       bHeater = false;
       vTaskDelay(PRINT_DELAY);
